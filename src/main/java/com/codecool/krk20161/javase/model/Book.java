469176d704d3d703813e9bda155cc9df4ce16bb0
klaudia borowska <canimi18@gmial.com>
2017-05-31 16:51:59 +0200

    Create class files

---
+++ src/main/java/com/codecool/krk20161/javase/model/Book.java
@@ -0,0 +1,9 @@
+package com.codecool.krk20161.javase.model;
+
+/**
+ * Created by KBorowska on 30.05.2017.
+ */
+public class Book {
+    public Book(String s, Author author1) {
+    }
+}
---
+++ src/main/java/com/codecool/krk20161/javase/model/Catalog.java
@@ -0,0 +1,7 @@
+package com.codecool.krk20161.javase.model;
+
+/**
+ * Created by KBorowska on 30.05.2017.
+ */
+public class Catalog {
+}
