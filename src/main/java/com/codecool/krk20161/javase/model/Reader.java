297dff965060a91e0202cc7fb2b692b2a20b0f9b
klaudia borowska <canimi18@gmial.com>
2017-05-31 17:31:47 +0200

    Create class constructor

--- src/main/java/com/codecool/krk20161/javase/model/Reader.java
+++ src/main/java/com/codecool/krk20161/javase/model/Reader.java
@@ -1,7 +1,22 @@
 package com.codecool.krk20161.javase.model;
 
+import java.util.ArrayList;
+
 /**
  * Created by KBorowska on 30.05.2017.
  */
-public class Reader {
+public class Reader extends Person{
+    private ArrayList<Book> booksList;
+
+    public Reader(String name) {
+        super(name);
+    }
+
+    public Reader(String name, ArrayList<Book> booksList) {
+        super(name);
+        this.setBooksList(ArrayList<Book>);
+    }
+
+
+
 }
