c078bc5654a99718bd7110403c3389cd40a7eeb8
klaudia borowska <canimi18@gmial.com>
2017-05-31 18:12:39 +0200

    Create set and get title

--- src/main/java/com/codecool/krk20161/javase/model/Book.java
+++ src/main/java/com/codecool/krk20161/javase/model/Book.java
@@ -16,4 +16,11 @@ public class Book {
         this.setAuthor(author);
         this.setSummary(summary);
     }
+
+    public void setTitle(String title){
+        this.title = title;
+    }
+    public String getTitle(){
+        return this.title;
+    }
 }
