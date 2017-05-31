e61fbf33b68e6739a506d9f1f0ab22455a26b9a2
klaudia borowska <canimi18@gmial.com>
2017-05-31 17:09:13 +0200

    Create constructor

--- src/main/java/com/codecool/krk20161/javase/model/Author.java
+++ src/main/java/com/codecool/krk20161/javase/model/Author.java
@@ -3,14 +3,11 @@ package com.codecool.krk20161.javase.model;
 /**
  * Created by KBorowska on 31.05.2017.
  */
-public class Author {
-    private String name;
+public class Author extends Person {
 
-    public Author(String name) {
-        this.name = name;
+    public Author(String name,String biography) {
+        super(name);
+        this.setBiography(biography);
     }
 
-    public String getName() {
-        return name;
-    }
 }
