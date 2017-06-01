adcf54134f3571d36bcd4b451f9446812fc9c98f
klaudia borowska <canimi18@gmial.com>
2017-06-01 10:52:17 +0200

    Change Person to abstract class

--- src/main/java/com/codecool/krk20161/javase/model/Person.java
+++ src/main/java/com/codecool/krk20161/javase/model/Person.java
@@ -3,7 +3,7 @@ package com.codecool.krk20161.javase.model;
 /**
  * Created by KBorowska on 31.05.2017.
  */
-public class Person {
+public abstract class Person {
     private String name;
 
     public Person(String name){
