4ac6c9bfc47e55374a2561077872a46fa5f23829
klaudia borowska <canimi18@gmial.com>
2017-06-01 10:49:50 +0200

    Create Library constracturo and get/set catalog method

--- src/main/java/com/codecool/krk20161/javase/model/Library.java
+++ src/main/java/com/codecool/krk20161/javase/model/Library.java
@@ -1,7 +1,23 @@
 package com.codecool.krk20161.javase.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by KBorowska on 30.05.2017.
  */
 public class Library {
+    private Catalog catalog;
+
+    public Library(){
+        this.setCatalog(catalog);
+    }
+
+    public void setCatalog(Catalog catalog){
+        this.catalog = catalog;
+    }
+
+    public Catalog getCatalog(){
+        return this.catalog;
+    }
 }
