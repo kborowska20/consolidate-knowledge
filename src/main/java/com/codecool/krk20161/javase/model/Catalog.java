d77fdf4a80a368b29b3dbf81a0bfa938c248130b
klaudia borowska <canimi18@gmial.com>
2017-06-01 10:58:30 +0200

    Change class to implements  searchBy

--- src/main/java/com/codecool/krk20161/javase/model/Catalog.java
+++ src/main/java/com/codecool/krk20161/javase/model/Catalog.java
@@ -6,7 +6,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 
-public class Catalog implements BookList {
+public class Catalog implements SearchBy {
     private List<Book> bookList;
 
     public Catalog(){
